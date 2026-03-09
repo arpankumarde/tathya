8c9f949e2c1447122486f3b4f2406800a095fec1
thtskaran <hello@karanprasad.com>
2026-03-09 15:53:27 +0530

    changes

--- server/main.py
+++ server/main.py
@@ -54,4 +54,4 @@ async def health():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, limit_max_requests=None)
+    uvicorn.run("main:app", host="0.0.0.0", port=8007, reload=True, limit_max_requests=None)
